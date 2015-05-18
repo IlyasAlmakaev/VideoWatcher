4e07762888dff77a1f9e8f370e8035c831918404
IlyasAlmakaev <i.almakaev@gmail.com>
2015-05-18 10:48:56 +0700

    Initial Commit

---
+++ VideoWatcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoWatcher
+//
+//  Created by almakaev iliyas on 18/05/15.
+//  Copyright (c) 2015 almakaev iliyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
