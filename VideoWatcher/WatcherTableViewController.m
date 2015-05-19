19d9fd0dcf6e64e7ac82592a2e8a7f051fe1ff5c
IlyasAlmakaev <i.almakaev@gmail.com>
2015-05-19 10:54:40 +0700

    parse name, descript

--- VideoWatcher/WatcherTableViewController.m
+++ VideoWatcher/WatcherTableViewController.m
@@ -55,7 +55,7 @@
 
 - (void)parse
 {
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.youtube.com/channel/UCtxxJi5P0rk6rff3_dCfQVw"]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.youtube.com/channel/UCtxxJi5P0rk6rff3_dCfQVw/videos"]];
     
     AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     
@@ -75,7 +75,11 @@
                                                        inManagedObjectContext:self.appD.managedOC];
              
              TFHppleElement *element;
-             NSString *test = [[[elements firstChildWithClassName:@"yt-lockup-title"]firstChildWithClassName:@"yt-uix-sessionlink yt-uix-tile-link  spf-link  yt-ui-ellipsis yt-ui-ellipsis-2"] objectForKey:@"title"];
+             NSString *name = [[[elements firstChildWithClassName:@"yt-lockup-title"]firstChildWithClassName:@"yt-uix-sessionlink yt-uix-tile-link  spf-link  yt-ui-ellipsis yt-ui-ellipsis-2"] objectForKey:@"title"];
+             
+             NSString *descript = [[elements firstChildWithClassName:@"yt-lockup-byline"]firstChildWithTagName:@"a"].text;
+             
+        //      NSString *descript = [[[elements firstChildWithClassName:@"yt-lockup-byline"]firstChildWithClassName:@"g-hovercard yt-uix-sessionlink yt-user-name  spf-link "] objectForKey:@"aria-label"];
              
       /*       self.news.title = [[[element firstChildWithClassName:@"topic-header"] firstChildWithClassName:@"topic-title word-wrap"] firstChildWithTagName:@"a"].text;
              
@@ -86,7 +90,7 @@
              self.news.reference = [[[[element firstChildWithClassName:@"topic-header"] firstChildWithClassName:@"topic-title word-wrap"] firstChildWithTagName:@"a"] objectForKey:@"href"];
              
              [self.newsContent addObject:self.news];*/
-             NSLog(@"%@", test);
+             NSLog(@"%@, %@", name, descript);
          }
          
     //     [self.tableView reloadData];
