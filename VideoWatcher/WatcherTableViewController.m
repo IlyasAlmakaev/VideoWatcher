7fdee0ecc34de1104f9de3c9bc8ff0020490cbb5
IlyasAlmakaev <intent@intents-iMac.local>
2015-05-21 10:22:05 -0700

    delete search trash

--- VideoWatcher/WatcherTableViewController.m
+++ VideoWatcher/WatcherTableViewController.m
@@ -202,38 +202,6 @@
 }
 
 #pragma mark -
-#pragma mark === Accessors ===
-#pragma mark -
-
-- (NSNumberFormatter *)decimalFormatter
-{
-    if (!_decimalFormatter)
-    {
-        _decimalFormatter = [[NSNumberFormatter alloc] init];
-        [_decimalFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
-    }
-    return _decimalFormatter;
-}
-
-- (NSFetchRequest *)searchFetchRequest
-{
-    if (_searchFetchRequest != nil)
-    {
-        return _searchFetchRequest;
-    }
-    
-    _searchFetchRequest = [[NSFetchRequest alloc] init];
-    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Video" inManagedObjectContext:self.managedObjectContext];
-    [_searchFetchRequest setEntity:entity];
-    
-    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
-    NSArray *sortDescriptors = [NSArray arrayWithObjects:sortDescriptor, nil];
-    [_searchFetchRequest setSortDescriptors:sortDescriptors];
-    
-    return _searchFetchRequest;
-}
-
-#pragma mark -
 #pragma mark === UISearchBarDelegate ===
 #pragma mark -
 
